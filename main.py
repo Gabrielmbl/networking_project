de0e4e8e3e6563af8ed5e7b7de6335aa743fed24
Gabrielmbl <gabrielmiranda7822@gmail.com>
2022-05-14 11:44:39 -0400

    Added

---
+++ main.py
@@ -0,0 +1,10 @@
+from Physical import *
+from DataLink import *
+if __name__ == '__main__':
+    layer1 = Physical()
+    layer1.receive()
+    #layer1.send('00', '11101011') # layer1.send('00', '111010110')
+    #layer1.send('01', '111011111')
+    
+
+
